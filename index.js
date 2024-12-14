56a9f828e9f37b64283c4c62779e4e401e99554b
[chrpkorir] <faythkoko@gmail.com>
2024-12-14 19:57:39 +0300

    Added filter method

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(drivers, name){
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase())
+
+}
+
+function fuzzyMatch(drivers, name){
+    return drivers.filter(driver => driver.toLowerCase().slice(0, name.length) === name.toLowerCase())
+    // slice(0, name.length) - it extracts substring from begining od drivers name upto
+    // the lenth of the name
+}
+
+function matchName(drivers, name){
+    return drivers.filter(driver => driver.name === name) // compares name of each driver object to the provided name
+
+}
